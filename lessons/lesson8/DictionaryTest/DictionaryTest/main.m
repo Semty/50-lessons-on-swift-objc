1b8efe7d53bd3cf830bd4947fac87792d26c6d3b
Semty <rustimchek@mail.ru>
2016-09-20 15:29:21 +0300

    added lesson8 in lessons

---
+++ lessons/lesson8/DictionaryTest/DictionaryTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DictionaryTest
+//
+//  Created by Руслан on 20.09.16.
+//  Copyright © 2016 Ruslan Timchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
