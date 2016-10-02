e552575a7fa89f0a3f27a14572b6fd7ec00c7706
Semty <rustimchek@mail.ru>
2016-10-02 14:09:39 +0300

    some fixes in HW13

--- Homework/Swift/HW13/HW13/AppDelegate.swift
+++ Homework/Swift/HW13/HW13/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        //                                                  *** LEARNER && STUDENT && MASTER && SUPERMAN ***
         let student1 = Student(name: "Ruslan(1)")
         let student2 = Student(name: "Dasha(2)")
         let student3 = Student(name: "Akram(3)")
