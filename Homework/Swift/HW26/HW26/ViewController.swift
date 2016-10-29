a3757e08de26a2e246b64e041b7ce864c399ef28
Semty <rustimchek@mail.ru>
2016-10-29 23:50:59 +0300

    added HW26 for swift and made some fixes to HW26 for obj-c

--- Homework/Swift/HW26/HW26/ViewController.swift
+++ Homework/Swift/HW26/HW26/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
+enum Animal: Int {
+    
+    case po
+    case tigress
+    case shifu
+    
+}
+
+enum Direction: Int {
+    
+    case up
+    case down
+    
+}
+
 class ViewController: UIViewController {
+    
+// MARK: - Storyboard Properties
     
     @IBOutlet weak var draggingView: UIImageView!
     
@@ -20,7 +37,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var speedAnimationLabel: UILabel!
     @IBOutlet weak var speedAnimationSlider: UISlider!
     
+// MARK: - Private properties
+    
+    private var isAnimation = false
+    private var speedAnimation: TimeInterval = 1
 
+// MARK: - viewDidLoad and didReceiveMemoryWarning
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,6 +54,93 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+// MARK: - Private mathods
+    
+    func move(to point: Direction) -> CGPoint {
+        
+        switch point {
+        case .up:
+            return CGPoint(x: self.draggingView.bounds.origin.x,
+                           y: self.draggingView.bounds.origin.y + 0.23 * self.draggingView.bounds.height)
+        case .down:
+            return CGPoint(x: self.draggingView.bounds.origin.x,
+                           y: self.draggingView.bounds.origin.y - 0.23 * self.draggingView.bounds.height)
+        }
+    }
+    
+    func transformDraggingView() {
+        
+        self.isAnimation = true
+        
+        var transform = CGAffineTransform.init(translationX: self.draggingView.bounds.origin.x,
+                                                            y: self.draggingView.bounds.origin.y)
+        transform = transform.rotated(by: 0)
+        transform = transform.scaledBy(x: 1, y: 1)
+        
+        if self.translationSwitch.isOn {
+            let point = move(to: Direction(rawValue: (Int)(arc4random()%2))!)
+            transform = transform.translatedBy(x: point.x, y: point.y)
+        }
+        
+        if self.rotationSwitch.isOn {
+            transform = transform.rotated(by: CGFloat(M_PI))
+        }
+        
+        if self.scaleSwitch.isOn {
+            transform = transform.scaledBy(x: 1.25, y: 1.25)
+        }
+        
+        UIView.animate(withDuration: self.speedAnimation,
+                       delay: 0,
+                       options: [.curveLinear, .allowUserInteraction, .beginFromCurrentState],
+                       animations: { self.draggingView.transform = transform },
+                       completion: ({(true) in
+                        
+                        UIView.animate(withDuration: self.speedAnimation,
+                                       delay: 0,
+                                       options: [.curveLinear, .allowUserInteraction, .beginFromCurrentState],
+                                       animations: { self.draggingView.transform = CGAffineTransform.identity },
+                                       completion: ({(true) in
+                                        
+                                        if self.scaleSwitch.isOn || self.rotationSwitch.isOn || self.translationSwitch.isOn {
+                                            self.transformDraggingView()
+                                        } else {
+                                            self.isAnimation = false
+                                        }
+                                        
+                                       }))
+                        }))
+    }
+    
+    func changeImage(_ animal: Animal) {
+        
+        switch animal {
+        case .po:
+            self.draggingView.image = UIImage.init(named: "Po.jpg")
+        case .tigress:
+            self.draggingView.image = UIImage.init(named: "Tigress.jpg")
+        case .shifu:
+            self.draggingView.image = UIImage.init(named: "Shifu.jpg")
+        }
+        
+    }
+    
+// MARK: - Actions
+    
+    @IBAction func actionSegmentedControl(_ sender: UISegmentedControl) {
+        changeImage(Animal(rawValue: sender.selectedSegmentIndex) ?? Animal.po)
+    }
+    
+    @IBAction func actionSlider(_ sender: UISlider) {
+        self.speedAnimation = TimeInterval(self.speedAnimationSlider.value)
+        self.speedAnimationLabel.text = String.init(format: "%1.1f", self.speedAnimation)
+    }
+    
+    @IBAction func actionSwitch(_ sender: UISwitch) {
+        if !self.isAnimation {
+            transformDraggingView()
+        }
+    }
 
 }
 
